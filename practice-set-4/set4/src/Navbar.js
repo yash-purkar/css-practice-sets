0ff50f09b5529f7020889ab207e21bf51ab8bada
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-05-13 09:30:48 +0530

    done set4

--- practice-set-4/set4/src/Navbar.js
+++ practice-set-4/set4/src/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './Navbar.css'
+
+
+
 
 export const Navbar = () => {
   return (
-    <div>
-      <NavLink to="/">Question1</NavLink> |
-      <NavLink to="/q2">Question2</NavLink> |
-      <NavLink to="/q3">Question3</NavLink> |
-      <NavLink to="/q4">Question4</NavLink> |
-      <NavLink to="/q5">Question5</NavLink>
-    </div>
+    <nav className='navigation'>
+      <NavLink to="/" className="nav-item">Question1</NavLink>
+      <NavLink to="/q2" className="nav-item">Question2</NavLink>
+      <NavLink to="/q3" className="nav-item">Question3</NavLink>
+      <NavLink to="/q4" className="nav-item">Question4</NavLink>
+      <NavLink to="/q5" className="nav-item">Question5</NavLink>
+    </nav>
   )
 }
--- practice-set-4/set4/src/Questions/Question5.js
+++ practice-set-4/set4/src/Questions/Question5.js
@@ -3,39 +3,44 @@ import '../Styles/Question5.css'
 import birds from '../Images/birds.jpg'
 export const Question5 = () => {
   return (
-    <div>
-      <h2>Review Order</h2>
-      <div className='item-detail'>
-        <div className='item-img'>
-          <h4>Item</h4>
-          <img src={birds} alt="product" width="50" height="50" />
+    <div className='containerq5'>
+      <div className='mainq5'>
+        <h2 className='heading'>Review Order</h2>
+        <div className='item-detail'>
+          <div className='item-img'>
+            <p className='heads font-bold'>Item</p>
+            <img src={birds} alt="product" width="50" height="50" />
+          </div>
+          <div className='qty-price-box'>
+
+            <div className='item-qty'>
+              <p className='heads font-bold'>Qty</p>
+              <p className='data'>1</p>
+            </div>
+            <div className="total-price">
+              <p className='heads font-bold'>Total</p>
+              <p className='data'>INR 999</p>
+            </div>
+          </div>
         </div>
-        <div className='item-qty'>
-          <h4>Qty</h4>
-          <p>1</p>
+        <div className="item-description">
+          <p className='desc'>Limited addition cactus print bag 1 litre, green color</p>
         </div>
-        <div className="total-price">
-          <h4>Total</h4>
-          <p>INR 999</p>
+        <div className='total-info'>
+          <p>Subtotal</p>
+          <p className='prices font-bold'>INR 999</p>
         </div>
+        <div className='total-info'>
+          <p>GST</p>
+          <p className='prices font-bold'>INR 179</p>
+        </div>
+        <div className='total-info grand-total'>
+          <p className='font-bold  grand-total'>Grand Total</p>
+          <p className='prices font-bold grand-price'>INR 1178</p>
+        </div>
+        <button className='place-order-btn'>Place order</button>
+        <p className='secured-by'>Cart Secured by Razorpay</p>
       </div>
-      <div className="item-description">
-        <p>Limited addition cactus print bag 1 litre, green color</p>
-      </div>
-      <div>
-        <p>Subtotal</p>
-        <p>INR 999</p>
-      </div>
-      <div>
-        <p>GST</p>
-        <p>INR179</p>
-      </div>
-      <div>
-        <p>Grand Total</p>
-        <p>INR 1178</p>
-      </div>
-      <button>Place order</button>
-      <p>Cart Secured by Razorpay</p>
     </div>
   )
 }
